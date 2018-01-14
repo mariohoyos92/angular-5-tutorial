3d8c4460fa4a29cdd86ab8a4dae7eafd3d528418
Mario Hoyos <mariohoyos92@gmail.com>
2018-01-14 13:33:25 -0600

    style: add animations for goals entering and leaving

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 
@@ -10,7 +11,12 @@ import { AboutComponent } from "./about/about.component";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
