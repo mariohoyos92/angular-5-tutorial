efa240b63b4941ca3127fae459a740bb2993e112
Mario Hoyos <mariohoyos92@gmail.com>
2018-01-14 13:54:08 -0600

    feat: add data service and connect home/about components

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,19 +2,29 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
 
+import { DataService } from "../data.service";
+
 @Component({
   selector: "app-about",
   templateUrl: "./about.component.html",
   styleUrls: ["./about.component.scss"]
 })
 export class AboutComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private _data: DataService
+  ) {
     this.route.params.subscribe(res => console.log(res.id));
   }
+
+  goals: any;
 
   sendMeHome() {
     this.router.navigate([""]);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._data.goal.subscribe(res => (this.goals = res));
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,8 @@ import {
   stagger
 } from "@angular/animations";
 
+import { DataService } from "../data.service";
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
@@ -66,21 +68,25 @@ export class HomeComponent implements OnInit {
   itemCount: number = 4;
   btnText: string = "Add an Item";
   goalText: string = "My first life goal";
-  goals = ["My first life goal", "I want to climb a mountain", "Go ice skiing"];
+  goals = [];
 
-  constructor() {}
+  constructor(private _data: DataService) {}
 
   ngOnInit() {
     this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => (this.goals = res));
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
     this.goals.push(this.goalText);
     this.goalText = "";
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
   removeItem(i) {
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 }
